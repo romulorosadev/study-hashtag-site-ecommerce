cc2c65d9ebf49dc76d6be4df8a3bc548dcf9e052
Romulo Rosa | @romulorosadev <romulorosadev@gmail.com>
2023-09-03 23:37:35 -0300

    js obj product e var card template

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,17 @@
-console.log("js ok");
+const product1 = {
+  name: "Casaco Off White",
+  brand: "Zara",
+  price: 90,
+  image: "product-1.jpg",
+  imageText: "Casaco de botão manga longa de cor off white",
+};
+
+const productCard = `
+  <div class="product__card" id="product__card--01">
+    <img class="product__image" src="src/img/${product1.image}" alt="${product1.imageText}" />
+    <p class="product__brand">${product1.brand}</p>
+    <p class="product__name">${product1.name}</p>
+    <p class="product__price">R$ ${product1.price},00</p>
+    <button class="product__buy">Comprar</button>
+  </div>
+`;
